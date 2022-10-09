894a7c6a6b9bf4f4e1c022efd1a1c0d788a9f85e
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-10-09 13:01:00 +0100

    SIGINT handler

---
+++ 0x06-signals/signals.h
@@ -0,0 +1,12 @@
+#ifndef _SIGNALS_H_
+#define _SIGNALS_H_
+
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int handle_signal(void);
+
+
+#endif /* _SIGNALS_H_ */
