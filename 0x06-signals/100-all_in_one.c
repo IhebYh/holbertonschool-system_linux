cf3d049fc4379ff192fba703b4c32edd1fb8f2a1
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-10-10 21:19:35 +0100

    a single handler for all signals

---
+++ 0x06-signals/100-all_in_one.c
@@ -0,0 +1,34 @@
+#include "signals.h"
+
+/**
+ * sigaction_handler - act as handler for sigaction syscall
+ * @signum: an integer representing a signal
+ * @info: a pointer to a siginfo_t with info about the signal
+ * @context: a void pointer to a user context (getcontext(3))
+ * Return: Always void
+ */
+void sigaction_handler(int signum, siginfo_t *info, void *context)
+{
+	char *msg = "Caught";
+
+	UNUSED(signum);
+	UNUSED(context);
+	psiginfo(info, msg);
+	fflush(stdout);
+}
+
+/**
+ * all_in_one - it sets up a single handler for all the signals
+ * Return: Always void
+ */
+void all_in_one(void)
+{
+	int i;
+	struct sigaction action;
+
+	action.sa_sigaction = sigaction_handler;
+	sigemptyset(&action.sa_mask);
+	action.sa_flags = SA_SIGINFO;
+	for (i = 1; i < NSIG; i++)
+		sigaction(i, &action, NULL);
+}
