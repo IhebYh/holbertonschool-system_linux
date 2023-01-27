8b9dd1368dd838027d5a74ac996d6046e791302b
Iheb Yahyaoui <iheb.yah@gmail.com>
2023-01-28 00:13:35 +0100

    thread entry

---
+++ 0x0A-multithreading/0-thread_entry.c
@@ -0,0 +1,14 @@
+#include "multithreading.h"
+
+/**
+ * thread_entry - Write a function that will serve as the entry
+ * point to a new thread.
+ * @arg: holds the address of a string that must be printed
+ * Return: void*
+ */
+void *thread_entry(void *arg)
+{
+	char *data = (char *) arg;
+	printf("%s\n", data);
+	pthread_exit(NULL);
+}
